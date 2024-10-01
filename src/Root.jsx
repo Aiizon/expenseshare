88dc8efc5c07bb158afd0a0c0829e13af22794b3
Aizon <killian.bonneau@iia-formation.fr>
2024-10-01 16:35:41 +0200

    Add functional router

---
+++ src/Root.jsx
@@ -0,0 +1,26 @@
+import {Link, Outlet} from "react-router-dom";
+
+export default function Root() {
+    return (
+        <>
+            <header className="bg-gray-800 text-white p-4 mb-4">
+                <div className="container mx-auto px-4">
+                    <div className="flex items-center justify-evenly">
+                        <h1 className="text-2xl">Expenseshare</h1>
+                        <nav>
+                            <ul className="flex gap-4">
+                                <li>
+                                    <Link to="/" className="text-blue-200 hover:text-white">Home</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </header>
+
+            <main className="container mx-auto px-4">
+                <Outlet />
+            </main>
+        </>
+    );
+}
